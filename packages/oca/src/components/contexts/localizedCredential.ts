83d81ecad5a270e8a573a6f5ca83ef28e07a9d73
Akiff Manji <amanji@petridish.dev>
2024-05-23 08:23:03 -0700

    feat: localized credential context
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

---
+++ packages/oca/src/components/contexts/localizedCredential.ts
@@ -0,0 +1,7 @@
+import { createContext, useContext } from 'react';
+
+import { LocalizedCredential } from '../../formatters/';
+
+export const LocalizedCredentialContext = createContext<LocalizedCredential | undefined>(undefined);
+
+export const useLocalizedCredential = () => useContext(LocalizedCredentialContext);
