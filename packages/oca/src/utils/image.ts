275f56845e02d4e0b664089a5267ed28e1c53dc3
Akiff Manji <amanji@petridish.dev>
2024-05-23 07:28:04 -0700

    feat: add image util function
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

---
+++ packages/oca/src/utils/image.ts
@@ -0,0 +1,8 @@
+import { ImageSourcePropType } from "react-native"
+
+export const toImageSource = (source: unknown): ImageSourcePropType => {
+  if (typeof source === 'string') {
+    return { uri: source as string }
+  }
+  return source as ImageSourcePropType
+}
