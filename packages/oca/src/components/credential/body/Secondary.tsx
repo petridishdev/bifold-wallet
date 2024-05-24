c06cbf9f06923a418f0a92d83f6f1f62b6a65f59
Akiff Manji <amanji@petridish.dev>
2024-05-23 18:51:06 -0700

    feat: credential secondary body
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

---
+++ packages/oca/src/components/credential/body/Secondary.tsx
@@ -0,0 +1,49 @@
+import { View, ImageBackground, StyleSheet, StyleProp, ViewStyle } from 'react-native'
+
+import { useLocalizedCredential } from '../../../components/contexts/localizedCredential';
+
+import { toImageSource } from '../../../utils/image';
+
+export interface SecondaryProps extends React.PropsWithChildren {
+  style?: StyleProp<ViewStyle>
+}
+
+export const Secondary: React.FC<SecondaryProps> = ({ style }) => {
+  const localizedCredential = useLocalizedCredential();
+  const { width, height } = style as ViewStyle;
+  const borderRadius = 10;
+
+  const styles = StyleSheet.create({
+    container: {
+      width: (width as number),
+      minHeight: (height as number),
+      borderTopLeftRadius: borderRadius,
+      borderBottomLeftRadius: borderRadius,
+      backgroundColor: localizedCredential?.secondaryBackgroundColor ?? localizedCredential?.primaryBackgroundColor,
+      zIndex: +!!(localizedCredential?.backgroundImageSlice ?? localizedCredential?.secondaryBackgroundColor),
+    },
+    image: {
+      flex: 1
+    }
+  })
+
+  return (
+    <View
+      // testID={testIdWithKey('CredentialCardSecondaryBody')}
+      style={[
+        styles.container
+      ]}
+    >
+      <ImageBackground
+        source={toImageSource(localizedCredential?.backgroundImageSlice)}
+        style={styles.image}
+        imageStyle={{
+          borderTopLeftRadius: borderRadius,
+          borderBottomLeftRadius: borderRadius,
+        }}
+      />
+    </View>
+  )
+}
+
+export default Secondary;
