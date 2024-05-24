d51866e113aae7c638869cec3dc6f594b7e24e33
Akiff Manji <amanji@petridish.dev>
2024-05-23 18:50:57 -0700

    feat: credential primary body
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

---
+++ packages/oca/src/components/credential/body/Primary.tsx
@@ -0,0 +1,34 @@
+import { View, StyleSheet, StyleProp, ViewStyle } from 'react-native'
+
+import { useLocalizedCredential } from '../../../components/contexts/localizedCredential';
+
+export interface PrimaryProps extends React.PropsWithChildren {
+  style?: StyleProp<ViewStyle>;
+}
+
+const Primary: React.FC<PrimaryProps> = ({ style, children }) => {
+  const localizedCredential = useLocalizedCredential();
+  const { width, height } = style as ViewStyle;
+  const borderRadius = 10;
+
+  const styles = StyleSheet.create({
+    container: {
+      width: width,
+      minHeight: height,
+      borderTopRightRadius: borderRadius,
+      borderBottomRightRadius: borderRadius,
+      backgroundColor: localizedCredential?.primaryBackgroundColor,
+      flex: 1
+    }
+  });
+
+  return (
+    <View
+      // testID={testIdWithKey('CredentialCardPrimaryBody')}
+      style={styles.container}>
+      {children}
+    </View>
+  )
+}
+
+export default Primary;
