1d7b2f35c38b9ac6e066f4a7360d61a0b909f89c
Akiff Manji <amanji@petridish.dev>
2024-05-24 22:48:32 -0700

    refactor: common styling
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- packages/oca/src/ui/components/card/utils.ts
+++ packages/oca/src/ui/components/card/utils.ts
@@ -1,3 +1,4 @@
+import { StyleSheet } from 'react-native'
 import { PADDING_RATIO, LOGO_RATIO, PRIMARY_RATIO, HEIGHT_RATIO, SECONDARY_RATIO } from './constants'
 
 export const calculatePadding = (width: number): number => {
@@ -27,3 +28,65 @@ export const calculateSecondaryWidth = (width: number): number => {
 export const calculateSecondaryHeight = (width: number): number => {
   return HEIGHT_RATIO * width
 }
+
+export const createStyleSheet = (windowWidth: number) => {
+    const padding = calculatePadding(windowWidth)
+    const logoWidth = calculateLogoWidth(windowWidth)
+    const logoHeight = calculateLogoHeight(windowWidth)
+    const primaryWidth = calculatePrimaryWidth(windowWidth)
+    const primaryHeight = calculatePrimaryHeight(windowWidth)
+    const secondaryWidth = calculateSecondaryWidth(windowWidth)
+    const secondaryHeight = calculateSecondaryHeight(windowWidth)
+  
+    const styles = StyleSheet.create({
+      container: {
+        flexDirection: 'row',
+        borderRadius: 10,
+        overflow: 'hidden',
+      },
+      logo: {
+        width: logoWidth,
+        height: logoHeight,
+        position: 'absolute',
+        top: padding,
+        left: padding,
+      },
+      secondary: {
+        width: secondaryWidth,
+        height: secondaryHeight,
+      },
+      primary: {
+        width: primaryWidth,
+        height: primaryHeight,
+      },
+      primaryChild: {
+        padding: padding,
+        paddingLeft: 2 * padding,
+        paddingRight: padding + logoWidth,
+      },
+      issuerText: {
+        color: ''
+      },
+      nameText: {
+        color: ''
+      },
+      watermark: {
+        width: windowWidth,
+        height: windowWidth,
+      },
+      watermarkText: {
+        fontSize: 0.05 * (windowWidth as number),
+        opacity: 0.16,
+        color: ''
+      },
+      status: {
+        width: logoWidth,
+        height: logoHeight,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+      },
+    })
+
+    return styles
+  }
--- packages/oca/src/ui/components/credential/Credential.tsx
+++ packages/oca/src/ui/components/credential/Credential.tsx
@@ -5,95 +5,46 @@ import Watermark from '@ui/components/card/Watermark'
 import Primary from '@ui/components/card/body/Primary'
 import Secondary from '@ui/components/card/body/Secondary'
 import Claim from '@ui/components/card/claim/Claim'
-import {
-  calculatePadding,
-  calculateLogoWidth,
-  calculateLogoHeight,
-  calculatePrimaryWidth,
-  calculatePrimaryHeight,
-  calculateSecondaryWidth,
-  calculateSecondaryHeight,
-} from '@ui/components/card/utils'
+import { createStyleSheet } from '@ui/components/card/utils'
 import { useCredentialTheme } from '@ui/contexts/credentialTheme'
 import { useLocalizedCredential } from '@ui/contexts/localizedCredential'
-import { StyleSheet, View, useWindowDimensions } from 'react-native'
+import { View, useWindowDimensions } from 'react-native'
 import Issuer from '@ui/components/card/Issuer'
 import Name from '@ui/components/card/Name'
 
 const Credential: React.FC = () => {
-  const { color, text } = useCredentialTheme()
+  const { color } = useCredentialTheme()
   const localizedCredential = useLocalizedCredential()
   const { width: windowWidth } = useWindowDimensions()
 
-  const padding = calculatePadding(windowWidth)
-  const logoWidth = calculateLogoWidth(windowWidth)
-  const logoHeight = calculateLogoHeight(windowWidth)
-  const primaryWidth = calculatePrimaryWidth(windowWidth)
-  const primaryHeight = calculatePrimaryHeight(windowWidth)
-  const secondaryWidth = calculateSecondaryWidth(windowWidth)
-  const secondaryHeight = calculateSecondaryHeight(windowWidth)
-
-  const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      borderRadius: 10,
-      overflow: 'hidden',
-    },
-    logo: {
-      width: logoWidth,
-      height: logoHeight,
-      position: 'absolute',
-      top: padding,
-      left: padding,
-    },
-    secondary: {
-      width: secondaryWidth,
-      height: secondaryHeight,
-    },
-    primary: {
-      width: primaryWidth,
-      height: primaryHeight,
-    },
-    primaryChild: {
-      padding: padding,
-      paddingLeft: 2 * padding,
-      paddingRight: padding + logoWidth,
-    },
+  let styles = createStyleSheet(windowWidth)
+  styles = {
+    ...styles,
     issuerText: {
-      color: contrastColor(
-        localizedCredential?.primaryBackgroundColor,
-        color.grayscale.darkGrey,
-        color.grayscale.white
-      ),
-    },
-    nameText: {
+      ...styles.issuerText,
       color: contrastColor(
         localizedCredential?.primaryBackgroundColor,
         color.grayscale.darkGrey,
         color.grayscale.white
       )
     },
-    watermark: {
-      width: windowWidth,
-      height: windowWidth,
+    nameText: {
+      ...styles.nameText,
+      color: contrastColor(
+        localizedCredential?.primaryBackgroundColor,
+        color.grayscale.darkGrey,
+        color.grayscale.white
+      )
     },
     watermarkText: {
-      fontSize: 0.05 * (windowWidth as number),
-      opacity: 0.16,
+      ...styles.watermarkText,
       color: contrastColor(
         localizedCredential?.primaryBackgroundColor,
         color.grayscale.darkGrey,
         color.grayscale.lightGrey
       ),
-    },
-    status: {
-      width: logoWidth,
-      height: logoHeight,
-      position: 'absolute',
-      top: 0,
-      right: 0,
-    },
-  })
+    }
+  }
 
   return (
     <View style={styles.container}>
