c3ffc9d341acb2119310de5585556b359ba66a91
Akiff Manji <amanji@petridish.dev>
2024-05-30 14:05:07 -0700

    feat: base styles
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

---
+++ packages/oca/src/ui/components/common/utils.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native'
+
+export const createBaseStyleSheet = (windowWidth: number) => {
+  const styles = StyleSheet.create({
+    container: {
+      overflow: 'hidden',
+    },
+    logo: {
+      position: 'absolute',
+    },
+    issuerText: {},
+    nameText: {},
+    claimText: {},
+    watermark: {
+      width: windowWidth,
+      height: windowWidth,
+    },
+    watermarkText: {
+      fontSize: 0.05 * (windowWidth as number),
+      opacity: 0.16,
+    },
+    status: {
+      position: 'absolute',
+      top: 0,
+      right: 0,
+    },
+  })
+
+  return styles
+}
