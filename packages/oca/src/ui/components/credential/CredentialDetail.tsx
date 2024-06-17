24a228f2a73271e560a58f7290037481ca2b21a6
Akiff Manji <amanji@petridish.dev>
2024-06-17 15:05:02 -0700

    chore: update i18n packages
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- packages/oca/src/ui/components/credential/CredentialDetail.tsx
+++ packages/oca/src/ui/components/credential/CredentialDetail.tsx
@@ -1,10 +1,26 @@
 import Detail from '@oca/ui/components/detail/Detail'
 import { useLocalizedCredential } from '@ui/contexts/localizedCredential'
 
-const CredentialDetail: React.FC = () => {
+import { I18nextProvider } from 'react-i18next'
+import { i18n } from 'i18next'
+import { initi18n } from '@oca/ui/i18n'
+
+interface CredentialDetailProps extends React.PropsWithChildren {
+  i18n?: i18n
+}
+
+const CredentialDetail: React.FC<CredentialDetailProps> = ({ i18n }) => {
   const localizedCredential = useLocalizedCredential()
 
-  return <Detail credential={localizedCredential} />
+  if (!i18n) {
+    i18n = initi18n()
+  }
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Detail credential={localizedCredential} />
+    </I18nextProvider>
+  )
 }
 
 export default CredentialDetail
--- packages/oca/src/ui/i18n.ts
+++ packages/oca/src/ui/i18n.ts
@@ -1,21 +1,23 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  resources: {
-    en: {
-      translation: {
-        Detail: {
-          Show: 'Show',
-          Hide: 'Hide',
-          ShowAll: 'Show All',
-          HideAll: 'Hide All',
+export const initi18n = () => {
+  i18n.use(initReactI18next).init({
+    lng: 'en',
+    fallbackLng: 'en',
+    resources: {
+      en: {
+        translation: {
+          Detail: {
+            Show: 'Show',
+            Hide: 'Hide',
+            ShowAll: 'Show All',
+            HideAll: 'Hide All',
+          },
         },
       },
     },
-  },
-})
+  })
 
-export default i18n
+  return i18n
+}
